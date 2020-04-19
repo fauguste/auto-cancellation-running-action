f26251cc882489ba3ecfc121140378c1cc35197e
fauguste <frederic.auguste@gmail.com>
2020-04-19 17:32:38 +0200

    erreor

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const context = github.context;
-const wait = require('./wait');
 var empty = require('is-empty');
 
 
