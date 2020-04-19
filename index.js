fc4b0cd391739fa8432307e8d27a10a33d91e48d
fauguste <frederic.auguste@gmail.com>
2020-04-19 13:15:36 +0200

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const context = github.context;
 const wait = require('./wait');
+var empty = require('is-empty');
 
 
 // most @actions toolkit packages have async methods
@@ -10,9 +11,15 @@ async function run() {
     const ms = core.getInput('milliseconds');
     console.log(`Waiting ${ms} milliseconds ...`)
     console.log(context);
+    const myToken = core.getInput('githubToken');
+    if(empty(myToken)) {
+      core.setFailed(`Action failed with error, please set githubToken token`);
+    }
     core.debug((new Date()).toTimeString())
 
     await wait(parseInt(ms));
+
+    const octokit = new github.GitHub(myToken);
     core.debug((new Date()).toTimeString())
 
     core.setOutput('time', new Date().toTimeString());
